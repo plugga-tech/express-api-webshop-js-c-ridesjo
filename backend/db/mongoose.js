35213018a7a33f88e4985780b547aeb4d79726fc
Camilla Ridesjö <camilla.ridesjo@gmail.com>
2023-03-24 16:20:38 +0100

    Add photos and more code

---
+++ backend/db/mongoose.js
@@ -0,0 +1,13 @@
+const { connect } = require('mongoose');
+(async () => {
+	try {
+		await connect(process.env.MONGODB_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useFindAndModify: false
+		});
+	} catch (error) {
+		console.log('Error', error);
+	}
+})();
